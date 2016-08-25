6b9fd66549bc04b93bab18b80bdbe8b13170ba1b
Wyatt Elliott <wyattelliott@Wyatts-MacBook-Pro.local>
2016-08-24 22:59:20 -0700

    styled

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+// allow these static directories to be used 
+
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/vendor", express.static(__dirname + '/vendor'));
+app.use("/views", express.static(__dirname + '/views'));
+
+
+
+
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('../index1');
 });
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
